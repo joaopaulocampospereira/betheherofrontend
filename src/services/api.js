613dcb289ebab5e05677c413d74aec01425ead79
joaopaulocampospereira <jaocampospereira@gmail.com>
2020-04-08 11:29:30 -0300

    netlify update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://be-the-hero-semanaomnistack.herokuapp.com/'
 })
 
 export default api;
